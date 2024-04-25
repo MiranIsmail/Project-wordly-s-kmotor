777baeb4a2b0760ec951f0f83d27d4c35a5adafb
MiranIsmail <Miim21@student.bth.se>
2024-04-25 09:19:10 +0200

    spin weeeeee

--- pws/src/navbar.js
+++ pws/src/navbar.js
@@ -1,20 +1,36 @@
 import "./navbar.css";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 
 function Navbar() {
+  const handleH3Click = (event) => {
+    const page = event.target.innerText.toLowerCase();
+    window.open = `localhost/${page}`;
+    console.log(page);
+  };
+  const handleLogoClick = (event) => {
+    window.open = "localhost";
+  };
+  const handleSearch = (event) => {
+    if (event.key === "Enter") {
+      const search = event.target.value.toLowerCase();
+      window.open = `localhost/search/${search}`;
+      event.target.value = "";
+    }
+  };
+
   return (
     <nav className="Home">
-      <img src={logo} alt="logo" className="logo" />
+      <img src={logo} onClick={handleLogoClick} alt="logo" className="logo" />
       <div className="menu">
         <div className="menu-items">
-          <h3>Home</h3>
-          <h3>Profile</h3>
-          <h3>Scoreboard</h3>
-          <h3>About</h3>
+          <h3 onClick={handleH3Click}>Home</h3>
+          <h3 onClick={handleH3Click}>Profile</h3>
+          <h3 onClick={handleH3Click}>Scoreboard</h3>
+          <h3 onClick={handleH3Click}>About</h3>
         </div>
         <div className="menu-space"></div>
       </div>
-      <input type="search" placeholder="  Search..." />
+      <input onKeyDown={handleSearch} type="search" placeholder="  Search..." />
     </nav>
   );
 }
